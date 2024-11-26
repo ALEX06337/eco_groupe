7e29631b28cc8b4062b101d83f71bc3dab59e737
Alexandre <alex06.diep@gmail.com>
2024-11-26 15:14:59 +0100

    maj des dossier inutile

--- Projet Prédiction/app.R
+++ Projet Prédiction/app.R
@@ -6,11 +6,78 @@ library(dplyr)
 
 
 
+
+
+
 source("data/bdd.R")
 day <- c(1, 7, 30, 90, 365)
 names(day) <- c('Jour', 'Semaine', 'Mois', 'Trimestre', 'An')
 
-source("ui.R")
+
+ui <- fluidPage(
+  titlePanel("Analyse et prédiction du cours des actions"),
+  hr(),
+  actionButton('help','Instruction Book'),
+  actionButton('yahoo','Yahoo', onclick = "window.open('https://fr.finance.yahoo.com/', '_blank')"),
+  br(),
+  br(),
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(
+        inputId = "entreprise",
+        label = "Tapez et sélectionnez :",
+        choices = unique(df1$Description),
+        options = list(
+          placeholder = "Commencez à taper...",
+          create = TRUE
+        )
+      ),
+      uiOutput('dynamic_var'),
+      
+      selectInput('period', "Choice your periode", choices = c('Jour', 'Semaine', 'Mois', 'Trimestre', 'An')),
+      
+      uiOutput('dynamic_period')
+      
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          "Global",
+          tabsetPanel(
+            tabPanel(
+              'Plot',
+              br(),
+              plotOutput("plot")
+            ),
+            tabPanel(
+              'Table',
+              br(),
+              DT::DTOutput("table")
+            )
+          )
+        ),
+        tabPanel(
+          "Prévision",
+          tabsetPanel(
+            tabPanel(
+              "Exponentiel",
+              br(),
+              plotOutput("expo")
+            ),
+            tabPanel(
+              "Action",
+              br(),
+              plotOutput("action")
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+
+
 
 server <- function(input, output) {
   
